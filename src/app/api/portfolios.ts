2b51427bb7f3003f33536f7756fbfa462f6cd4a9
handrianasolo <ahanitraelisoa@gmail.com>
2020-11-19 20:54:18 +0100

    data

---
+++ src/app/api/portfolios.ts
@@ -0,0 +1,50 @@
+export const PORTFOLIOS = [
+    {
+      id: 1,
+      src: 'assets/img/pat1.jpg',
+      title: 'Patisserie n°1',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+      id: 2,
+      src: 'assets/img/pat2.jpg',
+      title: 'Patisserie n°2',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+      id: 3,
+      src: 'assets/img/pat3.jpg',
+      title: 'Patisserie n°3',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+      id: 4,
+      src: 'assets/img/pat4.jpg',
+      title: 'Patisserie n°4',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+      id: 5,
+      src: 'assets/img/pat5.jpg',
+      title: 'Patisserie n°5',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+      id: 6,
+      src: 'assets/img/pat6.jpg',
+      title: 'Patisserie n°6',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+      id: 7,
+      src: 'assets/img/glace.jpg',
+      title: 'Patisserie n°7',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+      id: 8,
+      src: 'assets/img/pat7.jpg',
+      title: 'Patisserie n°8',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+]
