aeea3b1d2199f3414ce90230a30d299bd91b153b
handrianasolo <ahanitraelisoa@gmail.com>
2020-11-19 20:58:46 +0100

    routing for our components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioDetailsComponent } from './portfolio-details/portfolio-details.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'portfolios', component: PortfolioComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'portfolio-details/:id', component: PortfolioDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
